e8e9e6a871303f7aa651651458d1b24d56817029
Angel Zuniga <angel.zuna@icloud.com>
2023-08-09 17:33:53 -0500

    Changed color card to card container to edit and test the container

--- src/pages/configurer/index.tsx
+++ src/pages/configurer/index.tsx
@@ -1,9 +1,10 @@
 import ColorCard from 'colorz/components/ColorCard';
+import CardContainer from 'colorz/components/CardContainer';
 
 const configurer = () => {
     return (
         <div className="flex h-screen w-screen items-center justify-center">
-            <ColorCard colorHex="#FF00EA" />
+            <CardContainer />
         </div>
     );
 };
