4281755e16420546408f4cbeaa0270cb0fb73a77
Angel Zuniga <angel.zuna@icloud.com>
2023-08-09 17:31:15 -0500

    Added card shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
-import { type Config } from "tailwindcss";
+import { type Config } from 'tailwindcss';
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            boxShadow: {
+                card: '4px 4px 12px 0 rgba(0, 0, 0, 0.45)',
+            },
+        },
+    },
+    plugins: [],
 } satisfies Config;
